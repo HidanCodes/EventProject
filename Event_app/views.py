6ac67a603ac503ce7a8035b1ea7130a360d15a44
HidanCodes <HidanCodes@gmail.com>
2025-03-25 07:24:34 +0330

    view, get all events

--- Event_app/views.py
+++ Event_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from Event_app.models import Event
+from Event_app.serializers import EventSerializer
+
+
+class EventListCreateView(APIView):
+    """
+    نمایش لیست رویدادها و ایجاد رویداد جدید
+    """
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """
+        دریافت لیست تمام رویدادها
+        """
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
